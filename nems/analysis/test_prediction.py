a223bb9f1c085a15104f9f7af5ecfe705a3189fa
jacobpennington <jacob.pennington@wsu.edu>
2018-03-17 23:41:03 -0700

    Added more metrics to modelspec meta
    Added nmse and likelihood_poisson (from old nems master)
    to nems/analysis/test_prediction. Fixed issue with
    nmse expecting a dict of ndarrays instead of a recording,
    and fixed cost function definition in nems/analysis/fit_basic
    to match.

--- nems/analysis/test_prediction.py
+++ nems/analysis/test_prediction.py
@@ -1,23 +1,28 @@
 # For testing the predcitive accuracy of a set of modelspecs
-
-# TODO: Rework this code snippet to use actual metrics
-
-# Take your final measurements on the validation data set, and save!
-# results = {}
-# for m in nems.metrics.get_some_list_of_metric_functions:
-#    results[m.name] = m(val['resp'], evaluator(val, modelspec_fitted))
-
-import nems.modelspec as ms
-import nems.metrics.api
 import numpy as np
 
-def standard_correlation(est,val,modelspecs):
-    
+import nems.modelspec as ms
+import nems.metrics.api as nmet
+
+
+def standard_correlation(est, val, modelspecs):
+    # Evaluate estimation data, then store the results
+    # of metric tests in first modelspec's meta field.
     new_rec = [ms.evaluate(val, m) for m in modelspecs]
-    r_test = [nems.metrics.api.corrcoef(p, 'pred', 'resp') for p in new_rec]
+    r_test = [nmet.corrcoef(p, 'pred', 'resp') for p in new_rec]
+    mse_test = [nmet.nmse(p, 'pred', 'resp') for p in new_rec]
+    ll_test = [nmet.likelihood_poisson(p, 'pred', 'resp') for p in new_rec]
+    modelspecs[0][0]['meta']['r_test'] = np.mean(r_test)
+    modelspecs[0][0]['meta']['mse_test'] = np.mean(mse_test)
+    modelspecs[0][0]['meta']['ll_test'] = np.mean(ll_test)
+
+    # Repeat for validation data.
     new_rec = [ms.evaluate(est, m) for m in modelspecs]
-    r_fit = [nems.metrics.api.corrcoef(p, 'pred', 'resp') for p in new_rec]
-    modelspecs[0][0]['meta']['r_fit']=np.mean(r_fit)
-    modelspecs[0][0]['meta']['r_test']=np.mean(r_test)
-    
+    r_fit = [nmet.corrcoef(p, 'pred', 'resp') for p in new_rec]
+    mse_fit = [nmet.nmse(p, 'pred', 'resp') for p in new_rec]
+    ll_fit = [nmet.likelihood_poisson(p, 'pred', 'resp') for p in new_rec]
+    modelspecs[0][0]['meta']['r_fit'] = np.mean(r_fit)
+    modelspecs[0][0]['meta']['mse_fit'] = np.mean(mse_fit)
+    modelspecs[0][0]['meta']['ll_fit'] = np.mean(ll_fit)
+
     return modelspecs
---
+++ nems/metrics/loglike.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def likelihood_poisson(result, pred_name='pred', resp_name='resp'):
+    # TODO: Copied from old NEMS master, needs documentation.
+
+    x1 = result[pred_name].as_continuous()
+    x2 = result[resp_name].as_continuous()
+
+    # only keep indices where neither array is NaN
+    keepidx = np.isfinite(x1) * np.isfinite(x2)
+    x1 = x1[keepidx]
+    x2 = x2[keepidx]
+
+    # TODO: Why set this limit?
+    x1[x1 < 0.00001] = 0.00001
+
+    numer = np.mean(x2*np.log(x1))
+    denom = np.mean(x2)
+    return numer/denom
