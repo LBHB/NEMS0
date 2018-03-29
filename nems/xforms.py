f2659eba72f354b894bb0668222dbc136ab5b1a5
Stephen David <stephen.v.david@gmail.com>
2018-03-28 21:52:04 -0700

    tiny fix for xforms

--- nems/xforms.py
+++ nems/xforms.py
@@ -208,7 +208,7 @@ def generate_psth_from_est_for_both_est_and_val_nfold(est, val, **context):
 
 def init_from_keywords(keywordstring, meta={}, IsReload=False, **context):
     if not IsReload:
-        modelspec = init.from_keywords(keywordstring)
+        modelspec = init.from_keywords(keyword_string=keywordstring, meta=meta)
 
         return {'modelspecs': [modelspec]}
     else:
