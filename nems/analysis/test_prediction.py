4d934d4f3e663ff252901579a61589b043cb6409
crheller <charlieheller95@gmail.com>
2018-04-17 16:54:05 -0700

    added a couple of functions to evaluate model performance by jackknife set

--- nems/analysis/test_prediction.py
+++ nems/analysis/test_prediction.py
@@ -19,7 +19,6 @@ def generate_prediction(est,val,modelspecs):
 
     return new_est,new_val
 
-
 def standard_correlation(est,val,modelspecs):
 
     # Compute scores for validation data
@@ -41,4 +40,38 @@ def standard_correlation(est,val,modelspecs):
     modelspecs[0][0]['meta']['ll_fit'] = np.mean(ll_fit)
 
     return modelspecs
+
+def generate_prediction_sets(est,val,modelspecs):
+    if type(val) is list:
+        # ie, if jackknifing
+        new_est = [ms.evaluate(d, m) for m,d in zip(modelspecs,est)]
+        new_val = [ms.evaluate(d, m) for m,d in zip(modelspecs,val)]
+    else:
+        print('val and est must be lists')
+    
+    return new_est, new_val
+        
+def standard_correlation_by_set(est,val,modelspecs):
+
+    # Compute scores for validation data
+    r_test = [nmet.corrcoef(p, 'pred', 'resp') for p in val]
+    mse_test = [nmet.nmse(p, 'pred', 'resp') for p in val]
+    ll_test = [nmet.likelihood_poisson(p, 'pred', 'resp') for p in val]
+
+    # Repeat for est data.
+    r_fit = [nmet.corrcoef(p, 'pred', 'resp') for p in est]
+    mse_fit = [nmet.nmse(p, 'pred', 'resp') for p in est]
+    ll_fit = [nmet.likelihood_poisson(p, 'pred', 'resp') for p in est]
+    
+    
+    for i in range(len(modelspecs)):
+        modelspecs[i][0]['meta']['r_test'] = r_test[i]
+        modelspecs[i][0]['meta']['mse_test'] = mse_test[i]
+        modelspecs[i][0]['meta']['ll_test'] = ll_test[i]
+        
+        modelspecs[i][0]['meta']['r_fit'] = r_fit[i]
+        modelspecs[i][0]['meta']['mse_fit'] = mse_fit[i]
+        modelspecs[i][0]['meta']['ll_fit'] = ll_fit[i]
+        
+    return modelspecs 
 
