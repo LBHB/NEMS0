testing new masking system with TSP+behavior data, fixed bug in strf heatmap for mismatched wcc and fir dims

nems/analysis/fit_basic.py
@@ -175,7 +175,9 @@ def fit_nfold(data_list, modelspecs, generate_psth=False,
         else:
             msidx = 0
 
-        log.info("Fitting fold %d/%d from modelspec %d", i+1, nfolds, msidx)
+        log.info("Fitting fold %d/%d, modelspec %d (ftol=%e, maxit=%d)",
+                 i+1, nfolds, msidx, fit_kwargs['options']['ftol'],
+                 fit_kwargs['options']['maxiter'])
 #        resp = data_list[i]['resp']
 #        resp_len = np.sum(np.isfinite(resp.as_continuous()))
 #        log.info("non-nan resp samples: %d", resp_len)

nems/plots/heatmap.py
@@ -96,7 +96,11 @@ def strf_heatmap(modelspec, ax=None, clim=None, show_factorized=True,
     else:
         wc_coefs = np.array(wcc).T
         fir_coefs = np.array(firc)
-        strf = wc_coefs @ fir_coefs
+        if wc_coefs.shape[1] == fir_coefs.shape[0]:
+            strf = wc_coefs @ fir_coefs
+        else:
+            strf = fir_coefs
+            show_factorized = False
 
     if not clim:
         cscale = np.nanmax(np.abs(strf.reshape(-1)))