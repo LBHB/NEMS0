0c2b0d66c8d985ec00b40677f35c24200f8d71be
Christian Brodbeck <christianmbrodbeck@gmail.com>
2018-05-31 08:36:19 -0400

    get recordings

--- tests/test_load_recording.py
+++ tests/test_load_recording.py
@@ -8,77 +8,64 @@ Created on Thu Mar 29 09:26:27 2018
 
 import io
 import os
-import gzip
-import time
+from pathlib import Path
 import tarfile
-import requests
-import pandas as pd
-import numpy as np
-import copy
 import tempfile
 import shutil
 
-from nems.uri import local_uri, http_uri, targz_uri
-import nems.epoch as ep
-from nems.signal import RasterizedSignal, merge_selections, list_signals, load_signal, load_signal_from_streams
+from nems.signal import load_signal_from_streams
 from nems.recording import Recording
 
-#targz='/auto/data/nems_db/test/resp1.tgz'
-#rec1=load_recording_from_targz(targz)
-targz='/auto/data/nems_db/test/resp2.tgz'
-#rec2=load_recording_from_targz(targz)
 
-if os.path.exists(targz):
-    tgz_stream=open(targz, 'rb')
-else:
-    m = 'Not a .tar.gz file: {}'.format(targz)
-    raise ValueError(m)
+def test_load_recording():
+    targz = Path(__file__).parents[1] / 'recordings/resp2.tgz'
+    # tgz_stream = open(targz, 'rb')
 
-tpath=tempfile.mktemp()
 
-streams = {}  # For holding file streams as we unpack
-with tarfile.open(fileobj=tgz_stream, mode='r:gz') as t:
-    for member in t.getmembers():
-        if member.size == 0:  # Skip empty files
-            continue
-        basename = os.path.basename(member.name)
-        # Now put it in a subdict so we can find it again
-        signame = str(basename.split('.')[0:2])
-        if basename.endswith('epoch.csv'):
-            keyname = 'epoch_stream'
-            f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
+    tpath=tempfile.mktemp()
 
-        elif basename.endswith('.csv'):
-            keyname = 'data_stream'
-            f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
+    streams = {}  # For holding file streams as we unpack
+    with tarfile.open(targz, mode='r:gz') as t:
+        for member in t.getmembers():
+            if member.size == 0:  # Skip empty files
+                continue
+            basename = os.path.basename(member.name)
+            # Now put it in a subdict so we can find it again
+            signame = str(basename.split('.')[0:2])
+            if basename.endswith('epoch.csv'):
+                keyname = 'epoch_stream'
+                f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
 
-        elif basename.endswith('.h5'):
-            keyname = 'data_stream'
-            #f_in = io.BytesIO(t.extractfile(member).read())
+            elif basename.endswith('.csv'):
+                keyname = 'data_stream'
+                f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
 
-            # current non-optimal solution. extract hdf5 file to disk and then load
-            t.extract(member,tpath)
-            f=tpath+'/'+member.name
+            elif basename.endswith('.h5'):
+                keyname = 'data_stream'
 
-        elif basename.endswith('.json'):
-            keyname = 'json_stream'
-            f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
+                # current non-optimal solution. extract hdf5 file to disk and then load
+                t.extract(member,tpath)
+                f = tpath+'/'+member.name
 
-        else:
-            m = 'Unexpected file found in tar.gz: {} (size={})'.format(member.name, member.size)
-            raise ValueError(m)
-        # Ensure that we can doubly nest the streams dict
-        if signame not in streams:
-            streams[signame] = {}
-        # Read out a stringIO object for each file now while it's open
-        #f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
-        streams[signame][keyname] = f
+            elif basename.endswith('.json'):
+                keyname = 'json_stream'
+                f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
 
-# Now that the streams are organized, convert them into signals
-# log.debug({k: streams[k].keys() for k in streams})
-signals = [load_signal_from_streams(**sg) for sg in streams.values()]
-signals_dict = {s.name: s for s in signals}
+            else:
+                m = 'Unexpected file found in tar.gz: {} (size={})'.format(member.name, member.size)
+                raise ValueError(m)
+            # Ensure that we can doubly nest the streams dict
+            if signame not in streams:
+                streams[signame] = {}
+            # Read out a stringIO object for each file now while it's open
+            #f = io.StringIO(t.extractfile(member).read().decode('utf-8'))
+            streams[signame][keyname] = f
 
-rec = Recording(signals=signals_dict)
+    # Now that the streams are organized, convert them into signals
+    # log.debug({k: streams[k].keys() for k in streams})
+    signals = [load_signal_from_streams(**sg) for sg in streams.values()]
+    signals_dict = {s.name: s for s in signals}
 
-shutil.rmtree(tpath) # clean up
+    rec = Recording(signals=signals_dict)
+
+    shutil.rmtree(tpath) # clean up
