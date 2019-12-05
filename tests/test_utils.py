911b0b6c75075583a360fe78081058cb67296241
Alexander Tomlinson <mexander@gmail.com>
2019-12-05 11:51:08 -0800

    Awctomlinson/small fixes (#164)
    * improved escaped_split to use regex negative lookbehind for escape chars
    
    * add tests for escaped_split/join
    
    * simplify escaped_join; tests still pass
    
    * improved escaped_split to use regex negative lookbehind for escape chars
    
    * add tests for escaped_split/join
    
    * simplify escaped_join; tests still pass

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from nems.utils import escaped_split, escaped_join
 
+
 def test_split_and_join():
     s = 'my-escaped_modelspec-name_blah.t5\.5'
     x = escaped_split(s, '_')
@@ -12,6 +13,87 @@ def test_split_and_join():
     # of 2 instead of 3
     assert len(escaped_split(blah, '.')) == 2
 
-    # after the reverse operation, original string shouldl be preserved
+    # after the reverse operation, original string should be preserved
     y = escaped_join(x, '_')
     assert y == s
+
+
+def test_split_normal_delimiter():
+    splits = ['join', 'on', 'me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        s = d.join(splits)
+        split = escaped_split(s, d)
+        assert split == splits
+
+
+def test_split_escaped_delimiter():
+    splits = ['join', 'on\\', 'me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        s = d.join(splits)
+        split = escaped_split(s, d)
+        assert split == ['join', f'on\\{d}me', 'test']
+
+
+def test_join_normal_delimeter():
+    splits = ['join', 'on', 'me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        join = escaped_join(splits, d)
+        assert join == d.join(splits)
+
+
+def test_join_escaped_delimeter():
+    splits = ['join', 'on\\-', 'me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        join = escaped_join(splits, d)
+        assert join == d.join(splits)
+
+
+def test_join_escaped_delimeter2():
+    splits = ['join', 'on', '\\-me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        join = escaped_join(splits, d)
+        assert join == d.join(splits)
+
+
+def test_join_escaped_delimeter3():
+    splits = ['join', 'on', '\\--me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        join = escaped_join(splits, d)
+        assert join == d.join(splits)
+
+
+def test_join_escaped_delimeter4():
+    splits = ['join', 'on-', '\\-me', 'test']
+    delims = ['.', '_', '-']
+
+    for d in delims:
+        join = escaped_join(splits, d)
+        assert join == d.join(splits)
+
+
+def test_split_and_join_normal():
+    s = 'split_me_on_underscores'
+    assert escaped_join(escaped_split(s, '_'), '_') == s
+
+
+def test_split_and_join_escaped():
+    s = r'split_me_on\_underscores'
+    assert escaped_join(escaped_split(s, '_'), '_') == s
+
+    s = r'split_me_on_\_underscores'
+    assert escaped_join(escaped_split(s, '_'), '_') == s
+
+    s = r'split_me_on__\underscores'
+    assert escaped_join(escaped_split(s, '_'), '_') == s
