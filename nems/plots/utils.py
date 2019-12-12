b2fcc3813fcd130506400f5785180eb15560e7f7
Alexander Tomlinson <tomlinsa@ohsu.edu>
2019-12-12 13:55:39 -0800

    Add option to remove ticks/labels

--- nems/plots/utils.py
+++ nems/plots/utils.py
@@ -7,12 +7,20 @@ Created on Wed Sep  5 14:02:26 2018
 """
 import matplotlib.pyplot as plt
 
-def ax_remove_box(ax=None):
+
+def ax_remove_box(ax=None, right=True, top=True, left=False, bottom=False, ticks=False):
     """
     remove right and top lines from plot border
     """
     if ax is None:
         ax = plt.gca()
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
 
+    for remove, loc in [(right, 'right'), (left, 'left'), (top, 'top'), (bottom, 'bottom')]:
+        if remove:
+            ax.spines[loc].set_visible(False)
+
+    if ticks:
+        ax.set_xticks([])
+        ax.set_xticklabels([])
+        ax.set_yticks([])
+        ax.set_yticklabels([])
