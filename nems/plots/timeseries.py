c5dd4543ebcf476ab1d9bf6773051bc394cf68e9
Alexander Tomlinson <tomlinsa@ohsu.edu>
2019-12-12 12:20:50 -0800

    fix bug where some signals have channels=None causing len(None) to fail

--- nems/plots/timeseries.py
+++ nems/plots/timeseries.py
@@ -89,6 +89,9 @@ def timeseries_from_vectors(vectors, xlabel='Time', ylabel='Value', fs=None,
 def timeseries_from_signals(signals, channels=0,
                             no_legend=False, **options):
     """TODO: doc"""
+    if channels is None:
+        channels = 0
+
     channels = pad_to_signals(signals, channels)
 
     times = []
