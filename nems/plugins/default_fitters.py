c8b5fb2b2912a4764e28cb986c87fe2d83be7557
crheller <charlieheller95@gmail.com>
2022-01-17 02:04:26 -0800

    remove ddr pair plotting for ccnorm

--- nems/plugins/default_fitters.py
+++ nems/plugins/default_fitters.py
@@ -980,7 +980,7 @@ def ccnorm(fitkey):
 
     xfspec.append(['nems.xforms.predict', {}])
     xfspec.append(['nems.xforms.add_summary_statistics', {}])
-    xfspec.append(['nems_lbhb.stateplots.ddr_pairs', {}])
+    #xfspec.append(['nems_lbhb.stateplots.ddr_pairs', {}])
 
     return xfspec
 
